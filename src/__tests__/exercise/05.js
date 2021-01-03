e2506f9d8f35363caef602f117b6e50693e9e0d9
Bhavin789 <bhavib.789@gmail.com>
2021-01-03 17:18:40 +0530

    test: Update test 5 for RTL assertion

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -3,11 +3,15 @@
 
 import * as React from 'react'
 // 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
+import {waitForElementToBeRemoved} from '@testing-library/react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 // 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
+import {handlers} from '../../test/server-handlers'
 
 const buildLoginForm = build({
   fields: {
@@ -15,6 +19,8 @@ const buildLoginForm = build({
     password: fake(f => f.internet.password()),
   },
 })
+
+const server = setupServer(handlers[0])
 
 // 🐨 get the server setup with an async function to handle the login POST request:
 // 💰 here's something to get you started
@@ -25,6 +31,10 @@ const buildLoginForm = build({
 // you'll want to respond with an JSON object that has the username.
 // 📜 https://mswjs.io/
 
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
 // 🐨 before all the tests, start the server with `server.listen()`
 // 🐨 after all the tests, stop the server with `server.close()`
 
@@ -34,8 +44,81 @@ test(`logging in displays the user's username`, async () => {
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
-  // 🐨 uncomment this and you'll start making the request!
-  // userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'))
+
+  expect(screen.getByText(username)).toBeInTheDocument()
+
+  // as soon as the user hits submit, we render a spinner to the screen. That
+  // spinner has an aria-label of "loading" for accessibility purposes, so
+  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
+  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+
+  // once the login is successful, then the loading spinner disappears and
+  // we render the username.
+  // 🐨 assert that the username is on the screen
+})
+
+test(`Showing error message for incorrect parameters`, async () => {
+  const {container} = render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText('loading...'))
+
+  expect(screen.getByText('password required')).toBeInTheDocument()
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <form>
+        <div>
+          <label
+            for="username-field"
+          >
+            Username
+          </label>
+          <input
+            id="username-field"
+            name="username"
+            type="text"
+          />
+        </div>
+        <div>
+          <label
+            for="password-field"
+          >
+            Password
+          </label>
+          <input
+            id="password-field"
+            name="password"
+            type="password"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+      <div
+        style="height: 200px;"
+      >
+        <div
+          role="alert"
+          style="color: red;"
+        >
+          password required
+        </div>
+      </div>
+    </div>
+  `)
 
   // as soon as the user hits submit, we render a spinner to the screen. That
   // spinner has an aria-label of "loading" for accessibility purposes, so
